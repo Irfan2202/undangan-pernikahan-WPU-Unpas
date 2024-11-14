cecefcf4ce9e4e8b19843d5bb0ae1fcdf2d8fde0
Irfan2202 <112533797+Irfan2202@users.noreply.github.com>
2024-11-14 11:15:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+// Countdown
+simplyCountdown(".simply-countdown", {
+  year: 2024, // required
+  month: 12, // required
+  day: 30, // required
+  hours: 0, // Default is 0 [0-23] integer
+  minutes: 0, // Default is 0 [0-59] integer
+  seconds: 0, // Default is 0 [0-59] integer
+  words: {
+    //words displayed into the countdown
+    days: { singular: "hari", plural: "hari" },
+    hours: { singular: "jam", plural: "jam" },
+    minutes: { singular: "menit", plural: "menit" },
+    seconds: { singular: "detik", plural: "detik" },
+  },
+});
+
+// Offcanvas
+const stickyTop = document.querySelector(".sticky-top");
+const offcanvas = document.querySelector(".offcanvas");
+
+offcanvas.addEventListener("show.bs.offcanvas", function () {
+  stickyTop.style.overflow = "visible";
+});
+
+offcanvas.addEventListener("hidden.bs.offcanvas", function () {
+  stickyTop.style.overflow = "hidden";
+});
+
+// Disable scrolling
+
+const rootElement = document.querySelector(":root");
+const audioIconWrapper = document.querySelector(".audio-icon-wrapper");
+const audioIcon = document.querySelector(".audio-icon-wrapper i");
+const song = document.querySelector("#song");
+let isPlaying = false;
+
+function disableScroll() {
+  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+
+  window.onscroll = function () {
+    window.scrollTo(scrollTop, scrollLeft);
+  };
+}
+
+rootElement.style.scrollBehavior = "auto";
+
+function enableScroll() {
+  window.onscroll = function () {};
+  rootElement.style.scrollBehavior = "smooth";
+  // localStorage.setItem("opened", "true");
+  playAudio();
+}
+
+// play audio
+function playAudio() {
+  song.volume = 0.1;
+  audioIconWrapper.style.display = "flex";
+  song.play();
+  isPlaying = true;
+}
+
+audioIconWrapper.onclick = function () {
+  if (isPlaying) {
+    song.pause();
+    audioIcon.classList.remove("bi-disc");
+    audioIcon.classList.add("bi-pause-circle");
+  } else {
+    song.play();
+    audioIcon.classList.add("bi-disc");
+    audioIcon.classList.remove("bi-pause-circle");
+  }
+  isPlaying = !isPlaying;
+};
+
+// if (!localStorage.getItem("opened")) {
+//   disableScroll();
+// }
+disableScroll();
